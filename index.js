bf676a05e3a694e87216bbaae00a1a4e57f63f0f
AnttiVainikka <antti.vainikka36@gmail.com>
2024-06-07 08:24:45 +0300

    Finished 3.3

--- index.js
+++ index.js
@@ -2,26 +2,26 @@ const express = require('express')
 const app = express()
 
 
-const persons = [
+let persons = [
       {
         "name": "Arto Hellas",
         "number": "040-123456",
-        "id": "1"
+        "id": 1
       },
       {
         "name": "Ada Lovelace",
         "number": "39-44-5323523",
-        "id": "2"
+        "id": 2
       },
       {
         "name": "Dan Abramov",
         "number": "12-43-234345",
-        "id": "3"
+        "id": 3
       },
       {
         "name": "Mary Poppendieck",
         "number": "39-23-6423122",
-        "id": "4"
+        "id": 4
       }
 ]
 
@@ -33,6 +33,17 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+  
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
+
 app.get('/info', (request, response) => {
     response.send(`
     <div>
@@ -40,6 +51,12 @@ app.get('/info', (request, response) => {
     <br></br>
     ${Date()}
     </div>`)
+  })
+
+  app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
   })
 
 const PORT = 3001
